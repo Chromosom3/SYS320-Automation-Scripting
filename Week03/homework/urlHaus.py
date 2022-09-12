e4f3a4b977245fbc0698f800dfb484c19e61fd5c
Chromosom3 <36739730+Chromosom3@users.noreply.github.com>
2022-09-11 23:13:55 -0400

    Started working on fixing the code.

--- Week03/homework/urlHaus.py
+++ Week03/homework/urlHaus.py
@@ -1,10 +1,15 @@
-#
+# This imports the python CSV library
+# https://docs.python.org/3/library/csv.html
 import csv
-#
+# This defines a function called ur1HausOpen that takes two parameters.
 def ur1HausOpen(filename,searchTerm):
-#
-    while open('filename') as f:
-    contents == csv.review(filename)
+# Changed while to with as that is what the csv documentation states.
+# changed the f variable to csvfilename to help with keeping track.
+# Removed filename from quotes so it pulls the variable.
+    with open(filename) as csvfile:
+        # Changed review to reader as that is the right method for the CSV library.
+        # Also changed the value that it passes to the reader method as this is what the documentation says to do.
+        contents == csv.reader(csvfile)
     for _ in range(9):
     next(contents)
     for keyword in searchTerms:
