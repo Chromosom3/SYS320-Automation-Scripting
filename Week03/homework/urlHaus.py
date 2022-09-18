7dfd35d20446816ba426a6a1d56bccc3d615a2f5
Chromosom3 <36739730+Chromosom3@users.noreply.github.com>
2022-09-18 19:19:05 -0400

    Finished working on some doucmentation.

--- Week03/homework/urlHaus.py
+++ Week03/homework/urlHaus.py
@@ -1,10 +1,13 @@
 # This imports the python CSV library
-# https://docs.python.org/3/library/csv.html
+# 1. https://docs.python.org/3/library/csv.html
 import csv
 # Fix: Added this import to fix the x = re.findall() line not having re defined.
 # Imports the regular expression library.
 import re
 
+# 2. csv.reader(csvfile, delimiter='something_otherthan_a_comma')
+# 3. csv.reader(csvfile, escapechar='escape_char_value')
+# 4. A heredoc in python allows you to write a text block that preserves the line breaks and indintation. 
 
 # This defines a function called ur1HausOpen that takes two parameters.
 # Fix: there was a 1 instead of an l in the function name.
@@ -27,13 +30,14 @@ def urlHausOpen(filename,searchTerm):
         # Fix: Removed the s from searchTerms so it is the same as the paramater passed to the function.
         # Fix: Moved the indentation over for this section so it can be interpreted correctly.
         # Fix: Swapped the keyword in searchTerm and eachLine in contents for statements.
-        # This itterates through eachline in the CSV. 
+        # This itterates through eachline in the CSV after the 9 we already skipped. 
         for eachLine in contents:
-            # For each keyword in the search term
+            # This itterates through each keyword in the search term list passed to the function.
             for keyword in searchTerm:
                 # Fix: This is a non imported library. Had to add the import at the begining of the file.
                 # Fix: Fixed the arguments section to match the requirements for findall. 
                 # https://docs.python.org/3/library/re.html#re.findall
+                # This does a regular expression check for the keywork in the url field of the line.
                 x = re.findall(keyword,eachLine[2])
                 # Fix: Moved everything below here to this indentation level or further.
                 for _ in x:
@@ -46,8 +50,10 @@ def urlHausOpen(filename,searchTerm):
                     # CSV Header
                     # id,dateadded,url,url_status,last_online,threat,tags,urlhaus_link,reporter
                     # Fix: changed eachLine[4] (last_online) to eachLine[7] (urlhaus_link)
+                    # This sets the info src url to a variable to be printed
                     the_src = eachLine[7]
                     # Fix: Changed the string formating
+                    # Prints the information about the URL and the info.
                     print(f"""
                     URL: {the_url}
                     Info: {the_src}
