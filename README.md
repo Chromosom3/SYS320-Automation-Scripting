cb555cb5a25cd964c74dbb9fbfee18afddac1c06
Chromosom3 <36739730+Chromosom3@users.noreply.github.com>
2022-11-21 23:29:28 -0500

    homework

--- README.md
+++
@@ -1,2 +0,0 @@
-# SYS320-Automation-Scripting
-This is my repository for my SYS-320 Automation and Scripting class. 
--- update.bat
+++
@@ -1,3 +0,0 @@
-del E:\Repositories\SYS320-Automation-Scripting\Week14\homework\ransomware.ps1
-del .\ransomware_step2.ps1
-REM del backup.bat
